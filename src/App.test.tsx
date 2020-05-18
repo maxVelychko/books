d99bc1075430f855fca9c48e11dd2f94ba2a874e
Maksym Velychko <ismaxvelychko@gmail.com>
2020-05-18 14:39:04 +0300

    Add app skeleton

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the heading text correctly', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = getByText(/books/i);
+  
+  expect(headingElement).toBeInTheDocument();
 });
