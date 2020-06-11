0b79851cebe3d1ba7ad5d2688da9fa0a51385bd8
Maksym Velychko <ismaxvelychko@gmail.com>
2020-06-11 15:55:14 +0300

    Restructure app for hosting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import connectToDB from './db/mongoose';
 import bookRouter from './routers/book';
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3001;
 const app = express();
 connectToDB();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.json());
 app.use(bookRouter);
 
